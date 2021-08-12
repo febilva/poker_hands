5e5b0121bd2ab67ee0a60ca59b1eab8f059d6f46
febil va <febil.tvr@gmail.com>
2021-08-12 09:46:42 +0530

    cleanups

--- lib/poker_play.ex
+++ lib/poker_play.ex
@@ -1,31 +1,17 @@
 defmodule PokerPlay do
   alias PokerPlay.Comparer
+  @moduledoc false
 
-  @moduledoc """
-  Documentation for `PokerPlay`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> PokerPlay.hello()
-      :world
-
-  """
   def best_hand(lef_hand, right_hand) do
     left_hand_type =
       lef_hand
       |> PokerPlay.Card.init()
       |> PokerPlay.Hand.type()
-      |> IO.inspect(label: "hand_type_left")
 
     right_hand_type =
       right_hand
       |> PokerPlay.Card.init()
       |> PokerPlay.Hand.type()
-      |> IO.inspect(label: "hand_type_right")
 
     result = Comparer.compare(left_hand_type, right_hand_type)
 
